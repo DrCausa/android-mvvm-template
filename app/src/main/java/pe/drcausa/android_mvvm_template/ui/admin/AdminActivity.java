1a63545d8436106fa1a3ef63d67532a2108c60d0
DrCausa <leo10said@gmail.com>
2025-09-05 05:41:44 -0500

    feat(admin): initialize AdminActivity and set up fragment navigation
    - Added AdminActivity to AndroidManifest.xml.
    - Set initial fragment in AdminActivity to HomeFragment.
    - Added empty click handlers for Home, Users, Permissions, and Roles buttons.

--- app/src/main/java/pe/drcausa/android_mvvm_template/ui/admin/AdminActivity.java
+++ app/src/main/java/pe/drcausa/android_mvvm_template/ui/admin/AdminActivity.java
@@ -12,6 +12,7 @@ import androidx.core.view.WindowInsetsCompat;
 import com.google.android.material.button.MaterialButton;
 
 import pe.drcausa.android_mvvm_template.R;
+import pe.drcausa.android_mvvm_template.ui.menu.fragments.HomeFragment;
 import pe.drcausa.android_mvvm_template.utils.ActivityUtils;
 
 public class AdminActivity extends AppCompatActivity {
@@ -34,12 +35,24 @@ public class AdminActivity extends AppCompatActivity {
         btnPermissions.setOnClickListener(v -> handleBtnPermissions());
         btnRoles.setOnClickListener(v -> handleBtnRoles());
 
-        ActivityUtils.switchFragment(this, new AdminHomeFragment());
+        ActivityUtils.switchFragment(this, new HomeFragment());
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void handleBtnHome() {
+    }
+
+    private void handleBtnUsers() {
+    }
+
+    private void handleBtnPermissions() {
+    }
+
+    private void handleBtnRoles() {
     }
 }
